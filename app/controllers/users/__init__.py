b0a07e636b302079d72b8b14f1e6ca54c571fe23
Jordan Levin <jordan.levin@sap.com>
2021-01-16 11:29:12 -0800

    Document

--- app/controllers/users/__init__.py
+++ app/controllers/users/__init__.py
@@ -1,7 +1,7 @@
 import pymongo
-
+## Set up the users database connection and ensure the username field is indexed uniquely
 mongoClient = pymongo.MongoClient('mongo:27017')
-usersClient = mongoClient.engine["users"]
+usersClient = mongoClient.users["users"]
 usersClient.create_index(
     [("username", pymongo.DESCENDING)],
     unique=True
