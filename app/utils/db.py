e410ae7dc07536438a6fee1371edb40968635226
Jordan Levin <jordan.levin@sap.com>
2021-01-31 12:58:54 -0800

    Add debug for database

--- app/utils/db.py
+++ app/utils/db.py
@@ -1,8 +1,14 @@
 import pymongo
 import os
 
-mongoClient = {}
+mongoClient = None
 
 # Get Heroku MONGO_URL Config Var, else: use default of mongo:27017
-addr = os.getenv("MONGO_URL", "mongo:27017")
-mongoClient = pymongo.MongoClient(addr)
+try:
+
+    addr = os.getenv("MONGO_URL", "mongo:27017")
+    mongoClient = pymongo.MongoClient(addr)
+    print("Initialized data base connection with url: ", addr)
+    print("DB:", mongoClient)
+except:
+    print("Failed to establish database client")
