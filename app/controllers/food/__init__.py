2c582093396c29b80d67e3a022a0afb5d236495f
Jordan Levin <jordan.levin@sap.com>
2021-01-31 12:13:28 -0800

    Add spoonacular data ingestion

--- app/controllers/food/__init__.py
+++
@@ -1,9 +0,0 @@
-import pymongo
-from app.utils.db import mongoClient
-
-## Set up the database connection and ensure the appropriate field is indexed
-# usersClient = mongoClient.users["users"]
-# usersClient.create_index(
-#     [("username", pymongo.DESCENDING)],
-#     unique=True
-# )
--- app/controllers/recommendations/__init__.py
+++ app/controllers/recommendations/__init__.py
@@ -2,3 +2,25 @@ from app.utils.db import mongoClient
 
 ## Set up the recommendations database connection
 recsClient = mongoClient.recommendations["recommendations"]
+
+
+
+# Spoonacular Data Ingestion
+def ingestData():
+    API_TOKEN = "31badbba838549c59b1ca8f72c92d501"
+
+    recipesClient = mongoClient.recipes["recipes"]
+    recipe_id = 716430
+    for i in range(0, 100):
+        url = 'https://api.spoonacular.com/recipes/' + str(recipe_id) + '/information?apiKey=' + API_TOKEN + '&includeNutrition=true'
+        headers = {'Authorization': 'Bearer ' + API_TOKEN}
+
+        r = requests.get(url = url, headers = headers)
+        if r.status_code != 200:
+            continue
+
+        print("Ingesting data: ",r.json())
+        recipesClient.insert_one(r.json())
+        recipe_id = recipe_id+1
+
+ingestData() 
