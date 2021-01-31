86ab3b96730528410ca99bfeb5f3def5e1b6a376
Jordan Levin <jordan.levin@sap.com>
2021-01-31 12:38:22 -0800

    Add wsgi and remove run

--- wsgi.py
+++ wsgi.py
@@ -1,4 +1,10 @@
-from run import app
+from flask import Flask
+from werkzeug.contrib.fixers import ProxyFix
+from app import api
 
-if __name__ == "__main__":
-	app.run()
+app = Flask(__name__)
+api.init_app(app)
+
+# app.wsgi_app = middleware(app.wsgi_app)
+
+app.run(host="0.0.0.0", debug=True)
