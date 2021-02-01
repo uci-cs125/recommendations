55f10b67cb79d3ee66b4a579dedca714e4592ed2
Jordan Levin <jordan.levin@sap.com>
2021-01-31 16:06:30 -0800

    limit response size

--- app/controllers/recommendations/routes.py
+++ app/controllers/recommendations/routes.py
@@ -22,7 +22,7 @@ class RecommendationResource(Resource):
     @api.doc('list_recommendations')
     def get(self):
         try:
-            result = recsCollection.aggregate([{'$addFields': {"id": '$_id.oid'}}])
+            result = recsCollection.aggregate([{'$addFields': {"id": '$_id.oid'}}, { '$limit' : 20 }]) # only return the first 20 elements
             data = [doc for doc in result]
             print("data:", data)
             return craftResp(data, request, 200)
