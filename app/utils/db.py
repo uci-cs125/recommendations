f88f4f95709d38fd78bbc1d3727a18d2b981fb2a
Jordan Levin <jordan.levin@sap.com>
2021-02-15 11:30:32 -0800

    remove unneeded files

--- app/utils/db.py
+++ app/utils/db.py
@@ -2,13 +2,15 @@ import pymongo
 import os
 
 mongoClient = None
-
 # Get Heroku MONGO_URL Config Var, else: use default of mongo:27017
 try:
-
-    addr = os.getenv("MONGO_URL", "mongo:27017")
+    addr = os.getenv("MONGO_URL", "mongodb+srv://developer:developer@recommendations.g25bt.mongodb.net/<dbname>?retryWrites=true&w=majority")
     mongoClient = pymongo.MongoClient(addr)
     print("Initialized data base connection with url: ", addr)
-    print("DB:", mongoClient.server_info())
+    print("--------------------------------------------------------")
+    print("DB Initialized:")
+    print("--------------------------------------------------------")
+    print(mongoClient.server_info())
+
 except pymongo.errors.ServerSelectionTimeoutError as err:
     print("Failed to establish database client", err)
