0e724c368b9b15991d343b2f1b00bee8333e9af7
Jordan Levin <jordan.levin@sap.com>
2021-02-17 19:27:58 -0800

    limit 20

--- app/controllers/recipes/query_engine.py
+++ app/controllers/recipes/query_engine.py
@@ -12,7 +12,7 @@ class QueryEngine():
             mealType = "breakfast"
         elif context.currHour > 16:
             mealType = "dinner"
-        result = recipesCollection.aggregate([{ '$match': { 'dishTypes': { '$all': [mealType]}}}, { '$limit' : 1000 }]) # only return the first 20 elements
+        result = recipesCollection.aggregate([{ '$match': { 'dishTypes': { '$all': [mealType]}}}, { '$limit' : 20 }]) # only return the first 20 elements
         print("result:", result)
         data = [doc for doc in result]
         return data
